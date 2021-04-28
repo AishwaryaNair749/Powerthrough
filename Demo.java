0f4144f65ef14eb3ebb26542060badeb264b04d1
AishwaryaNair749 <47106562+AishwaryaNair749@users.noreply.github.com>
2021-04-29 00:00:17 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,19 @@
+class Addition
+{
+public static void main (String args[])
+{
+int a=10;
+float b=3.14f;
+float sum= a+b;
+char c = 'A';
+char d = 'a';
+System.out.println(2+5);
+System.out.println("Addition of " + a + " and " + b + " is " + sum);
+System.out.println("Addition of " + a + " and " + b + " is " + (a+b) );
+System.out.println("Addition of " + a + " and " + b + " is " + (a+(int)b) );
+System.out.println("Addition of " + a + " and " + b + " is " + a+b );
+System.out.println(c);
+System.out.println((int)c + " " + (int)d);
+System.out.println((char)124);
+}
+}
